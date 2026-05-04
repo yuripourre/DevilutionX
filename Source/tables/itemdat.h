bb3a7aa998b999940aabf68e8ec17d87a322f92b
Yuri Pourre <yuripourre@gmail.com>
2026-05-04 16:49:12 -0700

    Add DVL_API_FOR_TEST to UniqueItemMappingIdsToIndices

--- Source/tables/itemdat.h
+++ Source/tables/itemdat.h
@@ -654,7 +654,7 @@ extern DVL_API_FOR_TEST ankerl::unordered_dense::map<int32_t, int16_t> ItemMappi
 extern std::vector<PLStruct> ItemPrefixes;
 extern std::vector<PLStruct> ItemSuffixes;
 extern DVL_API_FOR_TEST std::vector<UniqueItem> UniqueItems;
-extern ankerl::unordered_dense::map<int32_t, int32_t> UniqueItemMappingIdsToIndices;
+extern DVL_API_FOR_TEST ankerl::unordered_dense::map<int32_t, int32_t> UniqueItemMappingIdsToIndices;
 
 tl::expected<_item_indexes, std::string> ParseItemId(std::string_view value);
 void LoadItemDatFromFile(DataFile &dataFile, std::string_view filename, int32_t baseMappingId);
